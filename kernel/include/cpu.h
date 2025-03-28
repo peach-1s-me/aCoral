ce1e8ba2e344a4135758b5e7bbb1e7c036c66501
rh-jiang <2047127819@qq.com>
2025-03-28 11:00:08 +0800

    [style] 内核cpu.c和.h规范代码风格

--- kernel/include/cpu.h
+++ kernel/include/cpu.h
@@ -2,34 +2,43 @@
  * @file cpu.h
  * @author 胡博文 (@921576434@qq.com)
  * @brief kernel层cpu头文件
- * @version 1.0
- * @date 2022-07-11
+ * @version 2.0
+ * @date 2025-03-28
  * 
- * @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2022 EIC-UESTC
  * 
  * @par 修订历史
  *     <table>
  *         <tr><th>版本 <th>作者 <th>日期 <th>修改内容
  *         <tr><td>v1.0 <td>胡博文 <td>2022-07-11 <td>增加注释
+ *         <tr><td>v2.0 <td>饶洪江 <td>2025-03-28 <td>规范代码风格
  */
 #ifndef KERNEL_CPU_H
 #define KERNEL_CPU_H
-#include <config.h>
-#include <type.h>
-#include <hal_comm.h>
+
+#include "config.h"
+#include "type.h"
+#include "hal_comm.h"
+
 #ifndef CFG_SMP
-///单核情况，只有一个核
+
+/* 单核情况，只有一个核 */ 
 #define acoral_current_cpu 0
-///单核情况，只有一个核
-#define acoral_idlest_cpu 0
-#else
-///重定义获取当前cpu函数
+/* 单核情况，只有一个核 */
+#define acoral_idlest_cpu  0
+
+#else /* ifndef CFG_SMP */
+
+/* 重定义获取当前cpu函数 */
 #define acoral_current_cpu HAL_GET_CURRENT_CPU()
-///重定义获取最空闲cpu函数
-#define acoral_idlest_cpu acoral_get_idlest_cpu()
-#endif
+/* 重定义获取最空闲cpu函数 */
+#define acoral_idlest_cpu  acoral_get_idlest_cpu()
+
+#endif /* ifndef CFG_SMP */
+
 acoral_u32 acoral_cpu_is_active(acoral_u32 cpu);
 void acoral_cpu_set_active(acoral_u32 cpu);
 acoral_u32 acoral_get_idlest_cpu(void);
 acoral_u32 acoral_get_idle_maskcpu(acoral_u32 cpu_mask);
+
 #endif
--- kernel/src/cpu.c
+++ kernel/src/cpu.c
@@ -3,21 +3,24 @@
  * @author 胡博文 (@921576434@qq.com)
  * @brief kernel层cpu源文件
  * @version 1.0
- * @date 2022-07-11
+ * @date 2025-03-28
  * 
- * @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2022 EIC-UESTC
  * 
  * @par 修订历史
  *     <table>
  *         <tr><th>版本 <th>作者 <th>日期 <th>修改内容
  *         <tr><td>v1.0 <td>胡博文 <td>2022-07-11 <td>增加注释
+ *         <tr><td>v2.0 <td>饶洪江 <td>2025-03-28 <td>规范代码风格
  */
-#include <cpu.h>
-#include <bitops.h>
-#include <lsched.h>
-///活动cpu位图
-acoral_u32 active_map[1]={0};
+#include "cpu.h"
+#include "bitops.h"
+#include "lsched.h"
+
+/* 活动cpu位图 */
+acoral_u32 acoral_active_map[1] = {0};
 extern acoral_rdy_queue_t acoral_ready_queues[CFG_MAX_CPU];
+
 /**
  * @brief 查看某cpu是否在活动状态
  * 
@@ -26,8 +29,9 @@ extern acoral_rdy_queue_t acoral_ready_queues[CFG_MAX_CPU];
  */
 acoral_u32 acoral_cpu_is_active(acoral_u32 cpu)
 {
-    return acoral_get_bit(cpu,active_map);
+    return acoral_get_bit(cpu, acoral_active_map);
 }
+
 /**
  * @brief 设置cpu为活动状态
  * 
@@ -35,7 +39,7 @@ acoral_u32 acoral_cpu_is_active(acoral_u32 cpu)
  */
 void acoral_cpu_set_active(acoral_u32 cpu)
 {
-	acoral_set_bit(cpu,active_map);
+	acoral_set_bit(cpu, acoral_active_map);
 }
 
 /**
@@ -45,36 +49,39 @@ void acoral_cpu_set_active(acoral_u32 cpu)
  */
 acoral_u32 acoral_get_idlest_cpu(void)
 {
-    acoral_u32 cpu,i,count=-1;
+    acoral_u32 cpu, i, count = 0xffffffff;
     acoral_rdy_queue_t *rdy_queue;
-    for(i=0;i<CFG_MAX_CPU;i++)
+
+    for (i = 0; i < CFG_MAX_CPU; i++)
     {
-        rdy_queue=acoral_ready_queues+i;
-        if(count>rdy_queue->array.num)
+        rdy_queue = acoral_ready_queues + i;
+        if (count > rdy_queue->array.num)
         {
-            count=rdy_queue->array.num;
-            cpu=i;
+            count = rdy_queue->array.num;
+            cpu = i;
         }
     }
     return cpu;
 }
+
 /**
  * @brief 获取最空闲的cpu（屏蔽）
  * 
- * @param cpu_mask 屏蔽
+ * @param cpu_mask 屏蔽cpu的掩码
  * @return acoral_u32 最空闲的cpu（屏蔽）
  */
 acoral_u32 acoral_get_idle_maskcpu(acoral_u32 cpu_mask)
 {
-    acoral_u32 cpu,i,count=-1;
+    acoral_u32 cpu, i, count = 0xffffffff;
     acoral_rdy_queue_t *rdy_queue;
-    for(i=0,cpu=0;i<CFG_MAX_CPU;i++)
+
+    for(i = 0, cpu = 0;i < CFG_MAX_CPU; i++)
     {
-        rdy_queue=acoral_ready_queues+i;
-        if(count>rdy_queue->array.num&&(1<<i&cpu_mask))
+        rdy_queue = acoral_ready_queues + i;
+        if ((count > rdy_queue->array.num) && ((1 << i) & cpu_mask))
         {
-            count=rdy_queue->array.num;
-            cpu=i;
+            count = rdy_queue->array.num;
+            cpu = i;
         }
     }
     return cpu;
