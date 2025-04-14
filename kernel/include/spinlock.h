93597db57ad301f9a3a1e57da60106e19c35e2c7
rh-jiang <2047127819@qq.com>
2025-04-14 10:45:37 +0800

    [style] spinlock.h、timer.c和timer.h规范代码风格

--- kernel/include/spinlock.h
+++ kernel/include/spinlock.h
@@ -2,44 +2,45 @@
  * @file spinlock.h
  * @author 胡博文 (@921576434@qq.com)
  * @brief kernel层自旋锁相关头文件
- * @version 1.0
- * @date 2022-07-08
- * 
- * @copyright Copyright (c) 2022
- * 
+ * @version 2.0
+ * @date 2025-04-14
+ *
+ * @copyright Copyright (c) 2022 EIC-UESTC
+ *
  * @par 修订历史
  *     <table>
  *         <tr><th>版本 <th>作者 <th>日期 <th>修改内容
  *         <tr><td>v1.0 <td>胡博文 <td>2022-07-08 <td>增加注释
+ *         <tr><td>v2.0 <td>饶洪江 <td>2025-04-14 <td>规范代码风格
  */
 #ifndef KERNEL_SPINLOCK_H
 #define KERNEL_SPINLOCK_H
-#include <config.h>
-#include <type.h>
-#include <hal_spinlock.h>
-///重定义自旋锁结构体，为上层使用
+#include "config.h"
+#include "type.h"
+#include "hal_spinlock.h"
+/* 重定义自旋锁结构体，为上层使用 */
 #define acoral_spinlock_t hal_spinlock_t
 
 #ifdef CFG_SMP
-///重定义自旋锁初始化
-#define acoral_spin_init(v) HAL_SPIN_INIT(v)
-///重定义自旋锁初始化（lock初始值为1）
+/* 重定义自旋锁初始化 */
+#define acoral_spin_init(v)      HAL_SPIN_INIT(v)
+/* 重定义自旋锁初始化（lock初始值为1） */
 #define acoral_spin_init_lock(v) HAL_SPIN_INIT_LOCK(v)
-///重定义自旋锁检测
+/* 重定义自旋锁检测 */
 #define acoral_spin_is_locked(v) HAL_SPIN_IS_LOCKED(v)
-///重定义自旋锁上锁（无阻塞）
-#define acoral_spin_try_lock(v) HAL_SPIN_TRY_LOCK(v)
-///重定义自旋锁上锁（有阻塞）
-#define acoral_spin_lock(v) HAL_SPIN_LOCK(v)
-///重定义自旋锁解锁
-#define acoral_spin_unlock(v) HAL_SPIN_UNLOCK(v)
-#else
+/* 重定义自旋锁上锁（无阻塞） */
+#define acoral_spin_try_lock(v)  HAL_SPIN_TRY_LOCK(v)
+/* 重定义自旋锁上锁（有阻塞） */
+#define acoral_spin_lock(v)      HAL_SPIN_LOCK(v)
+/* 重定义自旋锁解锁 */
+#define acoral_spin_unlock(v)    HAL_SPIN_UNLOCK(v)
+#else /* ifdef CFG_SMP */
 #define acoral_spin_init(v)
 #define acoral_spin_init_lock(v)
-#define acoral_spin_is_locked(v) 
-#define acoral_spin_try_lock(v) 
+#define acoral_spin_is_locked(v)
+#define acoral_spin_try_lock(v)
 #define acoral_spin_lock(v)
 #define acoral_spin_unlock(v)
-#endif
+#endif /* ifdef CFG_SMP */
 
 #endif
--- kernel/src/timer.c
+++ kernel/src/timer.c
@@ -2,48 +2,47 @@
  * @file timer.c
  * @author 胡博文 (@921576434@qq.com)
  * @brief kernel层时钟相关源文件
- * @version 1.0
- * @date 2022-07-12
- * 
- * @copyright Copyright (c) 2022
- * 
+ * @version 2.0
+ * @date 2025-04-14
+ *
+ * @copyright Copyright (c) 2022 EIC-UESTC
+ *
  * @par 修订历史
  *     <table>
  *         <tr><th>版本 <th>作者 <th>日期 <th>修改内容
  *         <tr><td>v1.0 <td>胡博文 <td>2022-07-12 <td>增加注释
+ *         <tr><td>v2.0 <td>饶洪江 <td>2025-04-14 <td>规范代码风格
  */
-#include <hal.h>
-#include <queue.h>
-#include <cpu.h>
-#include <int.h>
-#include <policy.h>
-#include <lsched.h>
-#include <timer.h>
-#include <print.h>
+#include "hal.h"
+#include "queue.h"
+#include "cpu.h"
+#include "int.h"
+#include "policy.h"
+#include "lsched.h"
+#include "timer.h"
+#include "print.h"
 #include "xscutimer.h"
 
-///时钟控制结构体
-static XScuTimer acoral_timer;
-///延时队列
-acoral_queue_t time_delay_queue;
-///基石时刻
-acoral_u32 ticks;
-
-void acoral_sched(void);
+/* 时钟控制结构体 */
+static XScuTimer      acoral_timer;
+/* 延时队列 */
+static acoral_queue_t time_delay_queue;
+/* 基石时刻 */
+static acoral_u32     ticks;
 
 /**
  * @brief 时钟管理系统初始化
- * 
+ *
  */
 void acoral_time_sys_init(void)
 {
-    //延时队列初始化
-	acoral_tick_queue_init(&time_delay_queue);
+    /* 延时队列初始化 */
+    acoral_tick_queue_init(&time_delay_queue);
 }
 
 /**
  * @brief 获得当前时刻
- * 
+ *
  * @return acoral_time 时刻
  */
 acoral_time acoral_get_ticks()
@@ -52,26 +51,26 @@ acoral_time acoral_get_ticks()
 }
 /**
  * @brief 设置时刻
- * 
+ *
  * @param time 要设置的时刻
  */
 void acoral_set_ticks(acoral_time time)
 {
-    ticks=time;
+    ticks = time;
 }
 
 /**
  * @brief 基石时钟中断函数
- * 
+ *
  * @param CallBackRef 回调参数
  */
 void acoral_ticks_handler(void *CallBackRef)
 {
 #ifdef CFG_OS_TICK_PRINT_ENABLE
     static acoral_time time_tmp = 0;
-    static acoral_u32  seconds = 0;
+    static acoral_u32 seconds   = 0;
 #endif
-    XScuTimer *TimerInstancePtr = (XScuTimer *) CallBackRef;
+    XScuTimer *TimerInstancePtr = (XScuTimer *)CallBackRef;
     if (XScuTimer_IsExpired(TimerInstancePtr))
     {
 #ifdef CFG_HOOK_TICKS
@@ -79,80 +78,93 @@ void acoral_ticks_handler(void *CallBackRef)
 #endif
         ticks++;
 #ifdef CFG_OS_TICK_PRINT_ENABLE
-        if((ticks - time_tmp)>=1000)
+        if ((ticks - time_tmp) >= 1000)
         {
             seconds++;
             time_tmp = ticks;
             acoral_print("\r\n----------OS Time: %ds----------\r\n", seconds);
         }
 #endif
-        if(acoral_sched_is_start()==true)
+        if (acoral_sched_is_start() == true)
         {
-            time_delay_deal();//延时链表处理
-            acoral_policy_time_deal();//调度处理
+            time_delay_deal();         /* 延时链表处理 */
+            acoral_policy_time_deal(); /* 调度处理 */
         }
-        //清楚定时器中断标志位
+        /* 清楚定时器中断标志位 */
         XScuTimer_ClearInterruptStatus(TimerInstancePtr);
     }
 }
 /**
  * @brief 基石时钟初始化
- * 
+ *
  */
 void acoral_ticks_init()
 {
-    ticks=0;//时刻初始化
+    ticks = 0; /* 时刻初始化 */
     XScuTimer_Config *timerConfig;
-    timerConfig = XScuTimer_LookupConfig(XPAR_SCUTIMER_DEVICE_ID);//获取时钟配置
-    XScuTimer_CfgInitialize(&acoral_timer, timerConfig, timerConfig->BaseAddr);//配置时钟
+    timerConfig = XScuTimer_LookupConfig(XPAR_SCUTIMER_DEVICE_ID);              /* 获取时钟配置 */
+    XScuTimer_CfgInitialize(&acoral_timer, timerConfig, timerConfig->BaseAddr); /* 配置时钟 */
 
-    XScuGic_Connect( &int_ctrl[0], XPAR_SCUTIMER_INTR, (Xil_ExceptionHandler) acoral_ticks_handler, ( void * ) &acoral_timer );//绑定中断服务函数
-    XScuGic_Enable( &int_ctrl[0], XPAR_SCUTIMER_INTR );//GIC使能时钟中断
+    /* 绑定中断服务函数 */
+    XScuGic_Connect(&int_ctrl[0],
+                    XPAR_SCUTIMER_INTR,
+                    (Xil_ExceptionHandler)acoral_ticks_handler,
+                    (void *)&acoral_timer
+                   );
+    XScuGic_Enable(&int_ctrl[0], XPAR_SCUTIMER_INTR); /* GIC使能时钟中断 */
 
-    XScuTimer_EnableAutoReload(&acoral_timer);//使能自动重装载
-    XScuTimer_SetPrescaler(&acoral_timer, 0);//设置时钟分频
-    XScuTimer_LoadTimer(&acoral_timer, XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 2000);//设置自动装载值
+    XScuTimer_EnableAutoReload(&acoral_timer);                                      /* 使能自动重装载 */
+    XScuTimer_SetPrescaler(&acoral_timer, 0);                                       /* 设置时钟分频 */
+    XScuTimer_LoadTimer(&acoral_timer, XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 2000); /* 设置自动装载值 */
 
-    XScuTimer_EnableInterrupt( &acoral_timer );//时钟使能中断
-    XScuTimer_Start(&acoral_timer);//启动时钟
-    acoral_intr_enable();//开中断
+    XScuTimer_EnableInterrupt(&acoral_timer); /* 时钟使能中断 */
+    XScuTimer_Start(&acoral_timer);           /* 启动时钟 */
+    acoral_intr_enable();                     /* 开中断 */
 }
 
 /**
-* @brief 将线程挂到延时队列上
-*
-* @param new 要添加的线程
-*/
+ * @brief 将线程挂到延时队列上
+ *
+ * @param new 要添加的线程
+ */
 void acoral_time_delay_queue_add(void *new)
 {
-	acoral_thread_t *thread = (acoral_thread_t *)new;
-	acoral_enter_critical();
-	acoral_tick_queue_add(&time_delay_queue, &thread->delaying);
+    acoral_thread_t *thread = (acoral_thread_t *)new;
+    acoral_enter_critical();
+    acoral_tick_queue_add(&time_delay_queue, &thread->delaying);
     acoral_suspend_thread(thread);
-	acoral_exit_critical();
-	return;
+    acoral_exit_critical();
+    return;
 }
 /**
  * @brief 延时处理函数
- * 
+ *
  */
 void time_delay_deal()
 {
-    acoral_list_t   *tmp,*tmp1,*head;
+    acoral_list_t *tmp, *tmp1, *head;
     acoral_thread_t *thread;
+
     head = &time_delay_queue.head;
-    if(acoral_list_empty(head))
-        return;
-    if(head->next->value>0)
-    	head->next->value--;
-    for(tmp=head->next;tmp!=head;)
+    if (acoral_list_empty(head))
     {
-        thread=list_entry(tmp,acoral_thread_t,delaying);
-        if(tmp->value>0)
+        return;
+    }
+    if (head->next->value > 0)
+    {
+        head->next->value--;
+    }
+
+    for (tmp = head->next; tmp != head;)
+    {
+        thread = list_entry(tmp, acoral_thread_t, delaying);
+        if (tmp->value > 0)
+        {
             break;
-        tmp1=tmp->next;
+        }
+        tmp1 = tmp->next;
         acoral_tick_queue_del(&time_delay_queue, &thread->delaying);
-        tmp=tmp1;
+        tmp  = tmp1;
         acoral_rdy_thread(thread);
     }
 }
