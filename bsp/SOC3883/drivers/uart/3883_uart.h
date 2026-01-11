ac7159afcd0d582a599257448b051633eabb40ac
JiayuanWen <648137125@qq.com>
2026-01-11 11:09:44 +0800

    [feat]增加SOC3883板级支持包

---
+++ bsp/SOC3883/drivers/uart/3883_uart.h
@@ -0,0 +1,21 @@
+/**
+ * @file 3883_uart.h
+ * @author 文佳源 (648137125@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-05
+ * 
+ * Copyright (c) 2025
+ * 
+ * @par 修订历史
+ * <table>
+ * <tr><th>版本 <th>作者 <th>日期 <th>修改内容
+ * <tr><td>v1.0 <td>文佳源 <td>2025-06-05 <td>内容
+ * </table>
+ */
+#ifndef _3883_UART_H
+#define _3883_UART_H
+
+
+
+#endif
---
+++ bsp/SOC3883/port/ash_port.c
@@ -0,0 +1,18 @@
+/**
+ * @file ash_port.c
+ * @author 文佳源 (648137125@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-05
+ * 
+ * Copyright (c) 2025
+ * 
+ * @par 修订历史
+ * <table>
+ * <tr><th>版本 <th>作者 <th>日期 <th>修改内容
+ * <tr><td>v1.0 <td>文佳源 <td>2025-06-05 <td>内容
+ * </table>
+ */
+
+
+ 
